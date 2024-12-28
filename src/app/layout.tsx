d3c9bbaa6311467ada1e33ab092cc4895f1e4a0d
pz2or <pub@ter05.net>
2024-12-28 11:45:35 +0900

    aa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { metadata as Meta } from "@/lib/metadata";
+import GoogleAdsense from "../components/adsense";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <GoogleAdsense pId="1268819614654241" />
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
