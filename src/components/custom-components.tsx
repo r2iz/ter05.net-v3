28a588b671174bfb930108b78f561cf2302a9256
pz2or <pub@ter05.net>
2025-01-25 22:10:24 +0900

    fix type

--- src/components/custom-components.tsx
+++ src/components/custom-components.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 interface CustomComponentProps {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 export const CustomH1 = ({ children }: CustomComponentProps) => (
